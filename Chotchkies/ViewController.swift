0da44ea70a3f228912361fbb9bd5c8240c379f71
marko <markocalvocruz@gmail.com>
2018-03-14 22:14:31 -0400

    Initial Commit

---
+++ Chotchkies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chotchkies
+//
+//  Created by Mark Calvo-Cruz on 3/14/18.
+//  Copyright © 2018 hello U. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
